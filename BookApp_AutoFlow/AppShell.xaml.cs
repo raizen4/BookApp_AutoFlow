6bfc282319ae33f79a792f1dc1e9ac27b7e4712d
Bboldur <110411230+Bboldur@users.noreply.github.com>
2024-05-19 00:05:44 +0100

    Initial MAUI app creation

---
+++ BookApp_AutoFlow/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BookApp_AutoFlow;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BookApp_AutoFlow/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BookApp_AutoFlow;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
